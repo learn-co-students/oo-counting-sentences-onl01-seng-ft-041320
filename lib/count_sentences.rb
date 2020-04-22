583b966ec4ae3207910bff38f0e9eba0d4a3d9f2
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-22 01:43:44 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? (".")
   end
 
   def question?
-
+self.end_with? ("?")
   end
 
   def exclamation?
-
+self.end_with? ("!")
   end
 
   def count_sentences
-
+   #complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+     self.split(/\.|\?|\!/).delete_if {|word| word.size < 2}.size
   end
 end
