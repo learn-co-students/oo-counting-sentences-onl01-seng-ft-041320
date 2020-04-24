46729ae75e0c6716b6ce95c0402fbe9951985aa8
Chibuisi Enyia <enyia21@gmail.com>
2020-04-24 20:27:14 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,14 +7,20 @@ class String
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    string_input = ""
+    count = 0
+    string_input = self
+    count = string_input.split(/\.|\?|\!\s/).length
   end
 end
+# strings = []
+# strings = "string? needs to end???? Now!!!  sjkhfkjd us stupid. lkdfjalkj done with this. okay?".split(". ")
+# print strings
